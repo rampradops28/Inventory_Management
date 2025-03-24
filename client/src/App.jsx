a569311bc3a80b458832228019fdeea95d7af259
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-24 13:35:44 +0530

    setup auth pages and dark mode using tailwind css ands shadCN

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import { ThemeProvider } from "./components/ui/theme-provider";
+
+// pages
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Forget_password from "./pages/Forget_password";
+import Reset_password from "./pages/Reset_password";
+import Verify_email from "./pages/Verify_email";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forget-password" element={<Forget_password />} />
+          <Route path="/reset-password" element={<Reset_password />} />
+          <Route path="/verify-email" element={<Verify_email />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,144 @@
+import { useState } from "react";
+import loginPng from "../assets/images/login.png";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ModeToggle } from "../components/mode-toggle";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  // schema for form validation
+  const formSchema = z.object({
+    email: z.string().email({
+      message: "Please enter a valid email address",
+    }),
+    password: z.string().min(4, {
+      message: "Please enter your password",
+    }),
+  });
+
+  // Initialize the form with react-hook-form and zod
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Handle submission
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
+
+  return (
+    <div className="min-h-screen grid sm:grid-cols-2 mx-auto justify-center items-center px-4">
+      <div className="mx-auto hidden sm:block">
+        <img src={loginPng} alt="login" className="max-w-full h-auto" />
+      </div>
+
+      <div className="mx-auto w-full max-w-md">
+        <div className="text-center mb-6">
+          <div className="flex">
+            <ModeToggle />
+          </div>
+          <h1 className="font-purple-purse text-4xl mb-2">Oh, You're back!</h1>
+          <h2 className="font-poppins text-2xl ">
+            Log in with your credentials
+          </h2>
+        </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <div className="relative">
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your Password"
+                          {...field}
+                          type={showPassword ? "text" : "password"}
+                        />
+                      </FormControl>
+                      <button
+                        type="button"
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <IoEyeOff className="h-5 w-5" />
+                        ) : (
+                          <IoEye className="h-5 w-5" />
+                        )}
+                      </button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                variant="default"
+                size="lg"
+                className="w-full"
+              >
+                Login
+              </Button>
+            </form>
+            <div className="flex mt-5 gap-5 justify-between mb-4">
+              <div className="flex">
+                <div>Not a member ?</div>
+                <div
+                  className="ml-2 cursor-pointer hover:underline "
+                  onClick={() => navigate("/signup")}
+                >
+                  Signup
+                </div>
+              </div>
+              <div
+                className="cursor-pointer text-red-400 hover:underline"
+                onClick={() => navigate("/forget-password")}
+              >
+                Forget Password ?
+              </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
